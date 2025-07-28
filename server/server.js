ab3127feef1836e010eea36ebf10b17958269b77
Vishesh0001 <shahvishesh64@gmail.com>
2025-07-28 17:44:21 +0530

    final commits

--- server/server.js
+++ server/server.js
@@ -1,9 +1,6 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const dotenv =require('dotenv')
 const app_routing = require('./modules/app-routing')
-
-// const path = require('path')
 const cors = require('cors');
 const { validateApiKey } = require('./middleware/header-validations');
 
@@ -14,34 +11,26 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 
-
 app.use(
   cors({
-    origin: 'http://localhost:3000', 
+    origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'token','api-key'], 
    
   })
 );
+app.get('/', (req, res) => {
+  res.send("Backend is running.");
+});
+
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 
 app.use(validateApiKey)
 
-app.use(bodyParser.text());
-app.use(bodyParser.json());
-const port = process.env.PORT;
-// app.use(validators.validateApiKey);
-// app.use(validators.extractHeaderLanguage);
-// app.use(validators.validateHeaderToken);
-
+const port = process.env.PORT || 1000;
   app_routing.v1(app);
-
-
-
-// common.getTaskDetail(8)
-// userModel.blogList()
 try {
   app.listen(port, () => {
   ;
